efef2afb5b152e229c19302aa5980fef87dc01fb
iamprovidence <orleon.nilkford@gmail.com>
2019-06-09 18:15:04 +0300

    salesman problem

---
+++ Genetic Algorithms/TravelingSalesmanProblem/SalesManTour/MainForm.cs
@@ -0,0 +1,272 @@
+﻿using System;
+
+using System.Diagnostics;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace SalesManTour
+{
+    public partial class MainForm : Form
+    {
+        // FIELDS
+        private bool isSolved;
+        private bool isRunning;
+        private bool doRegenerateMap;
+
+        private SmoothingMode smoothingMode;
+
+        private Point[] towns;
+        private ISolver<Tour> solver;
+        
+        private Random random;
+        private CancellationTokenSource cts;
+
+        private Stopwatch stopwatch;
+
+        // CONSTRUCTORS
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // turn on double buffered for panel
+            // to avoid glimmering
+            typeof(Control).GetProperty("DoubleBuffered", 
+                System.Reflection.BindingFlags.NonPublic | 
+                System.Reflection.BindingFlags.Instance  | 
+                System.Reflection.BindingFlags.SetProperty)
+                    .SetValue(pnlMap, true, null);
+
+            UpdateInterface();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // initialize fields
+            isSolved  = false;
+            isRunning = false;
+            doRegenerateMap = reGenCb.CheckState == CheckState.Checked;
+
+            smoothingMode = SmoothingMode.Default;
+
+            towns  = null;
+            solver = null;
+
+            random = new Random();
+            cts = null;
+
+            stopwatch = new Stopwatch();
+        }
+        private void UpdateInterface()
+        {
+            // disable on running
+            grbTowns.Enabled        = !isRunning;
+            grbParameters.Enabled   = !isRunning;
+            nudThreads.Enabled      = !isRunning;
+            pnlMap.Enabled          = !isRunning;
+
+            btnStart.Enabled        = towns != null && !isRunning;
+            btnStop.Enabled         = isRunning;
+        }
+        // DRAWING
+        #region DRAWING
+        private void Redraw()
+        {
+            pnlMap.Invalidate();
+        }
+        // handle event Paint
+        // Controls in Windows Forms are redraw in separate thread
+        // in this case we do not block user interface
+        private void pnlMap_Paint(object sender, PaintEventArgs e)
+        {
+            // gets graphics wich work in separate thread
+            Graphics canvas = e.Graphics;
+            canvas.SmoothingMode = smoothingMode;
+
+            // clear
+            Clear(canvas);
+
+            // draw
+            Tour bestTour = solver?.Best();
+            if (bestTour != null)            DrawTour(canvas, bestTour);
+            if (towns != null)               DrawMap(canvas);
+            if (towns != null && isSolved)   DrawTowns(canvas);
+        }
+        private void Clear(Graphics graphics)
+        {
+            graphics.Clear(Color.White);
+        }
+        private void DrawTour(Graphics canvas, Tour tour)
+        {
+            lblLength.Text = $"Довжина туру{tour.Length(),10:#.000}";
+            canvas.DrawLines(Pens.Black, tour.Route());
+        }
+        private void DrawMap(Graphics canvas)
+        {
+            for (int i = 0; i < towns.Length; ++i)
+            {
+                Rectangle town = new Rectangle(towns[i].X - 4, towns[i].Y - 4, 7, 7);
+                canvas.DrawEllipse(Pens.Black, town);
+                canvas.FillEllipse(Brushes.White, town);
+            }
+        }
+        private void DrawTowns(Graphics canvas)
+        {
+            Font font = new Font("Microsoft Sans Serif", 8);
+
+            // first town
+            canvas.DrawString("1", font, Brushes.Red, new Point(towns[0].X - 4, towns[0].Y + 3));
+            canvas.FillEllipse(Brushes.Red, new Rectangle(towns[0].X - 4, towns[0].Y -4, 7, 7));
+            
+            // others towns
+            for (int i = 1; i < towns.Length; ++i)
+            {
+                canvas.DrawString((i + 1).ToString(), font, Brushes.Black,
+                    new Point(towns[i].X - 5, towns[i].Y + 3));
+            }
+        }
+        #endregion
+
+        // INTERFACE LOGIC
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            // get input data
+            isSolved = false;
+
+            int x = pnlMap.ClientSize.Width-10;
+            int y = pnlMap.ClientSize.Height-10;
+            int mapSize = (int)nudNumber.Value;
+
+            // create random towns
+            towns = new Point[mapSize];
+            for (int i = 0; i < towns.Length; ++i)
+            {
+                towns[i] = new Point(random.Next(x) + 5, random.Next(y) + 5);
+            }
+            Tour.towns = towns;
+
+            // reset solver
+            solver = null;
+
+            // update view
+            Redraw();
+            UpdateInterface();
+            // clear genetic algorithms result
+            lblStopwatch.Text   = "Минуло";
+            lblGenerations.Text = "Перевірено 0 поколінь";
+            lblLength.Text      = "Довжина туру";
+        }
+        
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            // run and update view
+            isRunning = true;
+            UpdateInterface();
+
+            // start genetic algorithm
+            stopwatch.Restart();
+
+            solver = MakeSolver(threadsAmount: (int)nudThreads.Value);
+            await RunGeneticAlgorithmAsync(solver);
+
+            // end genetic algorithm
+            stopwatch.Stop();
+            isRunning = false;
+
+            // update view
+            Redraw();
+            UpdateInterface();
+        }
+        private async Task RunGeneticAlgorithmAsync(ISolver<Tour> solver)
+        {
+            isSolved = false;
+
+            // create NEW cancelation source
+            cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            token.Register(UpdateInterface);
+
+            // track progress ..
+            Progress<int> progressHandler = new Progress<int>(value =>
+            {
+                // .. and show it
+                lblStopwatch.Text   = "Минуло " + stopwatch.Elapsed.ToString(@"mm\:ss\.ff");
+                lblGenerations.Text = $"Перевірено {value} поколінь";
+
+                // do not block user interface
+                // let Forms update interface in its onw thread
+                Redraw();
+            });
+
+            // run solver
+            await Task.Run(() => 
+            {
+                solver.Solve(token, progressHandler);
+            });
+
+            isSolved  = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // cancel
+            cts?.Cancel();
+            stopwatch.Stop();
+            
+            // update field
+            isSolved  = true;
+            isRunning = false;
+        }
+
+        private void MainForm_ResizeEnd(object sender, EventArgs e)
+        {
+            if (doRegenerateMap && !isRunning)
+            {
+                btnGenerate.PerformClick();
+            }
+        }
+
+        private void reGenCb_CheckedChanged(object sender, EventArgs e)
+        {
+            doRegenerateMap = reGenCb.CheckState == CheckState.Checked;
+        }
+        
+        private void ToolStripQualityMenuItem_Click(object sender, EventArgs e)
+        {
+            string itemText = (sender as ToolStripMenuItem)?.Text;
+
+            switch (itemText)
+            {
+                case "High": smoothingMode = SmoothingMode.HighQuality; break;
+                case "Low":  smoothingMode = SmoothingMode.Default;     break;
+            }
+        }
+        // almost a factory
+        private ISolver<Tour> MakeSolver(int threadsAmount)
+        {
+            if (threadsAmount == 1)
+            {
+                return new SSolver(
+                    populationSize:     (int)nudSize.Value,
+                    mutationCount:      (int)nudMutate.Value,
+                    routeMutationCount: (int)nudRotate.Value,
+                    maxGeneration:      (int)nudGenMax.Value * 1000);
+            }
+            else
+            {
+                return new PSolver(
+                    threadsCount:       threadsAmount,
+                    populationSize:     (int)nudSize.Value,                    
+                    mutationCount:      (int)nudMutate.Value,
+                    routeMutationCount: (int)nudRotate.Value,
+                    maxGeneration:      (int)nudGenMax.Value * 1000);
+            }
+        }
+
+    }
+}
---
+++ Genetic Algorithms/TravelingSalesmanProblem/SalesManTour/Tour.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManTour
+{
+    /// <summary>
+    /// Клас інкапсулює послідовність відвідання міст, координати яких задано зовні.
+    /// Має методи "мутування" для створення нових турів, вміє обчислити довжину маршруту.
+    /// Потребує допомоги зовнішнього генератора випадкових чисел.
+    /// </summary>
+    class Tour
+    {
+        // Посилання на карту - колекцію кординат міст
+        static public Point[] towns;
+        // Перестановка номерів міст - послідовність відвідання
+        private int[] order;
+
+        // За замовчуванням міста відвідують послідовно від першого до останнього
+        public Tour()
+        {
+            order = Enumerable.Range(0, towns.Length).ToArray();
+        }
+        // Випадкова перестановка послідовності міст
+        public Tour(Random random)
+        {
+            order = Enumerable.Range(0, towns.Length).ToArray();
+            random.Shuffle(order);
+        }
+        // Визначена перестановка послідовності міст
+        public Tour(int[] nums)
+        {
+            order = nums;
+        }
+
+        // Довжина маршруту - сума довжин прямолінійних відрізків між сусідніми містами маршруту.
+        // Довжина відрізку - відстань між точками на координатній площині.
+        public double Length()
+        {
+            int n = towns.Length - 1;
+            double L = Math.Sqrt(Math.Pow(towns[order[n]].X - towns[order[0]].X, 2) + 
+                Math.Pow(towns[order[n]].Y - towns[order[0]].Y, 2));
+            for (int i = 0; i < n; ++i)
+                L += Math.Sqrt(Math.Pow(towns[order[i]].X - towns[order[i + 1]].X, 2) + 
+                    Math.Pow(towns[order[i]].Y - towns[order[i + 1]].Y, 2));
+            return L;
+        }
+
+        // Проста мутація: обмін місцями двох випадкових міст у послідовності
+        public Tour Mutate(Random random)
+        {
+            int[] nums = (int[])this.order.Clone();
+            int k = random.Next(order.Length);
+            int l = random.Next(order.Length);
+            int val = nums[k];
+            nums[k] = nums[l];
+            nums[l] = val;
+            return new Tour(nums);
+        }
+
+        // Мутація напрямку: змінює на протилежний напрям руху між двома випадковими містами
+        public Tour Rutate(Random random)
+        {
+            int[] nums = (int[])this.order.Clone();
+            int n = order.Length;
+            int k = random.Next(n);
+            int l = random.Next(n);
+            if (k == l) return new Tour();
+            int val = 0;
+            if (k < l)
+                while (k < l)
+                {
+                    val = nums[k];
+                    nums[k] = nums[l];
+                    nums[l] = val;
+                    ++k; --l;
+                }
+            else
+                while (k > l)
+                {
+                    val = nums[k];
+                    nums[k] = nums[l];
+                    nums[l] = val;
+                    ++k; k %= n;
+                    --l; l = (l < 0) ? n - 1 : l;
+                }
+            return new Tour(nums);
+        }
+
+        public Point[] Route()
+        {
+            int n = order.Length;
+            Point[] R = new Point[n + 1];
+            for (int i = 0; i < n; ++i)
+                R[i] = towns[order[i]];
+            R[n] = towns[order[0]];
+            return R;
+        }
+    }
+
+    // Підказка з інтернету, що розширює можливості генератора випадкових чисел:
+    //  випадкове перемішування послідовності (масиву) довільних об'єктів
+    static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}
